17ad8bf09c950fec0b5955bbbc6a0c6d67de0021
Kloenk <kloenk@gmx.de>
2016-10-08 12:17:44 +0200

    add Update starter

--- GModServer/SendStartMail.py
+++ GModServer/SendStartMail.py
@@ -6,12 +6,14 @@ def sendStartEMail(steamWorkshopId, serverGamemode, serverdefaultMap, serverMaxP
     from eMail.StartTextFile import SendStartEMail
     SendStartEMail(steamWorkshopId, serverGamemode, serverdefaultMap, serverMaxPlayer, serverPort)
 
-def StartServer(steamApiAuthKey=SteamApiAuthKey, steamWorkShopId=SteamWorkShopId, serverGamemode=ServerGamemode, serverDefaultMap=ServerDefaultMap, serverMaxPlayer=ServerMaxPlayer, serverPort=ServerPort):
+def StartServer(steamApiAuthKey=SteamApiAuthKey, steamWorkShopId=SteamWorkShopId, serverGamemode=ServerGamemode, serverDefaultMap=ServerDefaultMap, serverMaxPlayer=ServerMaxPlayer, serverPort=ServerPort, debug=False):
     print("start")
     sendStartEMail(steamWorkShopId, serverGamemode, serverDefaultMap, serverMaxPlayer, serverPort)
+    from GModServer.UpdateAndValidateServer import updateGarrysModServer
+    updateGarrysModServer(debug=debug)
 
 
 
 if __name__ == '__main__':
     from PythonServerKernel.Exceptions import RunnedFromFalseFile
-    raise RunnedFromFalseFile('startServer_py')
+    raise RunnedFromFalseFile('GModServer_SendStartMail_py')
